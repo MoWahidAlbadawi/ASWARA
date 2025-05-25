d5657a5c9e5a9664249c7c014d13f6c9ff713a5a
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2025-05-25 09:09:29 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+    <div className="test-app">
+      <h2>I am developer Wahid </h2>
+      <p>welcome to jewelry store</p>
+      </div>
+      <div>
+        <p>Techs I installed for this projects for now</p>
+        <ul>
+          <li></li>
+        </ul>
+        <p>what is next ?</p>
+      </div>
+    </>
+  )
+}
+export default App;
