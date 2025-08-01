7346d3775cbdab70bbe748e60ccb0f4ea06b7a32
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2025-08-01 16:19:18 +0300

    fix (layout dashboard) : layout style

--- src/pages/Dashboard/LayoutDashboard.tsx
+++ src/pages/Dashboard/LayoutDashboard.tsx
@@ -5,18 +5,23 @@ import { Outlet } from "react-router-dom";
 // menu context
 import { useContext } from "react";
 import { MenuContext } from "@/context/MenuContext";
+
 const LayoutDashboard = () => {
+
     const ctxMenu = useContext(MenuContext);
-    const sideBarClasses = `${ctxMenu.showMenu ? 'left-0' : '-left-full'} h-screen absolute top-0 lg:static shadow-md bg-white z-50 border-r border-gray-300`;
+    const sideBarClasses = `${ctxMenu.showMenu ? 'left-0' : '-left-full lg:left-0'} h-screen fixed top-0  shadow-md bg-white z-50 border-r border-gray-300`;
+    const contentClasses = `${ctxMenu.showMenu ? 'left-0 lg:left-[21%] xl:left-[16%] w-full lg:w-[79%] xl:w-[84%]' : 'left-0 lg:left-[8%] xl:left-[6%] w-full lg:w-[92%] xl:w-[94%] '} 
+             transition-all duration-300 ease-in-out absolute top-0 flex flex-col gap-6 min-h-screen`;
+    
     return (
         // main Box
-        <Box className='flex gap-6 min-h-screen'>
+        <Box className='min-h-screen'>
             {/* sidebar */}
-            <Box className={sideBarClasses}>
+        <Box className={sideBarClasses}>
                 <Sidebar />
         </Box>
             {/* navbar & page */}
-            <Box className='flex flex-col gap-6 w-full min-h-screen'>
+            <Box className={contentClasses}>
             {/* navbar */}
             
             <Box className='h-[70px] shadow-md rounded w-full'>
--- src/pages/Website/Home.tsx
+++ src/pages/Website/Home.tsx
@@ -1,10 +1,47 @@
-import { Link } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Cookie from 'cookie-universal'
+import toast from "react-hot-toast";
 
 const Home = () => {
-    return <div>
-        <p>Home page here</p>
-        <Link to='/login'>Login page</Link>
-        <Link to='/register'>Register page</Link>
+    const cookies = Cookie();
+    const token = cookies.get('aswara');
+    const navigate = useNavigate();
+
+    function navigateToLogin () {
+        if(token) {
+            toast.error('you are already logged in');
+            return;
+        }
+        else {
+            navigate('/login');
+        }
+    }
+
+    function navigateToDashboard () {
+        if(!token) {
+            toast.error('you must login first!');
+            return;
+        }
+        else {
+            navigate('/aswaraDashboard/home');
+        }
+    }
+
+
+    return <div className="flex flex-col gap-5 justify-center items-center !mt-12">
+        <Typography component={'h2'} className="!text-2xl text-primary-dark">Welcome to the ASWARA</Typography>
+        <Typography className="!text-xl text-secondary-main">Wahid makes it now</Typography>
+        <div className="flex flex-col gap-5 !mt-3">
+        <Button variant="contained" className="!text-white" sx={{textTransform : 'none'}}
+        onClick={navigateToLogin}>
+            Go To Login
+        </Button>
+        <Button variant="contained" className="!text-white" sx={{textTransform : 'none'}}
+        onClick={navigateToDashboard}>
+            Go To Dashboard
+        </Button>
+        </div>
         </div>
 }
 export default Home;
